f9dc14ffe2c0b7457f177eb4249ff546f7b5df06
Krecios <Krecios@users.noreply.github.com>
2020-12-03 09:05:17 +0100

    Implemented the random path method

--- pathfinding.py
+++ pathfinding.py
@@ -1,16 +1,18 @@
+import random
+
 labirynth = []
 startCoordinates = []
 endCoordinates = []
 wallCoordinates = []
 
 
-def readLabirynth():
+def ReadLabirynth():
     file = open("labirinth.txt", "r")
     for line in file:
         labirynth.append(line[:-1])
 
 
-def whereIsStart():
+def WhereIsStart():
     rowCounter = 0
     collumnCounter = 0
     for line in labirynth:
@@ -18,13 +20,13 @@ def whereIsStart():
             if char == "S":
                 print("The start of the labirinth is a coordinates: X: " +
                       str(rowCounter) + " Y: " + str(collumnCounter))
-                return (rowCounter, collumnCounter)
+                return [rowCounter, collumnCounter]
             rowCounter = rowCounter + 1
         rowCounter = 0
         collumnCounter = collumnCounter + 1
 
 
-def whereIsEnd():
+def WhereIsEnd():
     rowCounter = 0
     collumnCounter = 0
     for line in labirynth:
@@ -32,13 +34,13 @@ def whereIsEnd():
             if char == "E":
                 print("The end of the labirinth is a coordinates: X: " +
                       str(rowCounter) + " Y: " + str(collumnCounter))
-                return (rowCounter, collumnCounter)
+                return [rowCounter, collumnCounter]
             rowCounter = rowCounter + 1
         rowCounter = 0
         collumnCounter = collumnCounter + 1
 
 
-def whereAreWalls():
+def WhereAreWalls():
     listOfWalls = []
     rowCounter = 0
     collumnCounter = 0
@@ -49,11 +51,49 @@ def whereAreWalls():
             rowCounter = rowCounter + 1
         rowCounter = 0
         collumnCounter = collumnCounter + 1
-    print(listOfWalls)
     return listOfWalls
 
 
-readLabirynth()
-startCoordinates = whereIsStart()
-endCoordinates = whereIsEnd()
-wallCoordinates = whereAreWalls()
+def IsCoordinateAWall(coordinate):
+    for coord in wallCoordinates:
+        if(coordinate == coord):
+            return True
+    return False
+
+
+def RandomWalk():
+    currentCoordinates = startCoordinates
+    path = []
+    while True:
+        path.append(currentCoordinates)
+        available = []
+        availableCount = 0
+        if currentCoordinates == endCoordinates:
+            print("Exit found")
+            print(path)
+            return
+        right = [currentCoordinates[0], currentCoordinates[1] + 1]
+        left = [currentCoordinates[0], currentCoordinates[1] - 1]
+        up = [currentCoordinates[0] - 1, currentCoordinates[1]]
+        down = [currentCoordinates[0] + 1, currentCoordinates[1]]
+        if IsCoordinateAWall(right) == False:
+            available.append(right)
+            availableCount = availableCount + 1
+        if IsCoordinateAWall(left) == False:
+            available.append(left)
+            availableCount = availableCount + 1
+        if IsCoordinateAWall(up) == False:
+            available.append(up)
+            availableCount = availableCount + 1
+        if IsCoordinateAWall(down) == False:
+            available.append(down)
+            availableCount = availableCount + 1
+        currentCoordinates = available[random.randint(0, availableCount - 1)]
+
+
+ReadLabirynth()
+startCoordinates = WhereIsStart()
+endCoordinates = WhereIsEnd()
+print(endCoordinates)
+wallCoordinates = WhereAreWalls()
+RandomWalk()
