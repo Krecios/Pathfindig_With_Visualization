38acef8f9329dc7bc87fde3e253323701d7599eb
Krecios <Krecios@users.noreply.github.com>
2020-12-03 12:58:53 +0100

    Enchanced the randomWalk and added the output creation

--- pathfinding.py
+++ pathfinding.py
@@ -4,12 +4,22 @@ labirynth = []
 startCoordinates = []
 endCoordinates = []
 wallCoordinates = []
+visitedCoordinates = []
+wholePath = []
+rowCount = 0
+collumnCount = 0
 
 
-def readLab():
+def readLabirinth():
     file = open("labirinth.txt", "r")
     for line in file:
         labirynth.append(line[:-1])
+
+
+def getDimensions():
+    rowCount = len(labirynth[0])
+    collumnCount = len(labirynth)
+    print("Rows : " + str(rowCount) + ", Collumns: " + str(collumnCount))
 
 
 def findStart():
@@ -61,39 +71,81 @@ def isWall(coordinate):
     return False
 
 
+def isVisited(currentPath, analyzedCoords):
+    for coord in currentPath:
+        if coord == analyzedCoords:
+            return True
+    return False
+
+
+def checkSolution(path):
+    for coord in path:
+        counter = 0
+        for coordCheck in path:
+            if coord == coordCheck:
+                counter = counter + 1
+            if counter > 1:
+                print("ERROR")
+                return
+    print("Correct")
+
+
+def drawSolution():
+    file = open("output.txt", "w")
+    for y in range(12):
+        for x in range(24):
+            if [x, y] == startCoordinates:
+                file.write("S")
+            elif [x, y] == endCoordinates:
+                file.write("E")
+            elif isWall([x, y]) == True:
+                file.write("1")
+            elif isVisited(wholePath, [x, y]) == True:
+                file.write("V")
+            else:
+                file.write("0")
+        file.write("\n")
+
+
 def randomWalk():
     currentCoordinates = startCoordinates
     path = []
     while True:
-        path.append(currentCoordinates)
         available = []
         availableCount = 0
         if currentCoordinates == endCoordinates:
-            print("Exit found")
+            print("Path found:")
             print(path)
-            return
+            checkSolution(path)
+            return path
         right = [currentCoordinates[0], currentCoordinates[1] + 1]
         left = [currentCoordinates[0], currentCoordinates[1] - 1]
         up = [currentCoordinates[0] - 1, currentCoordinates[1]]
         down = [currentCoordinates[0] + 1, currentCoordinates[1]]
-        if isWall(right) == False:
+        if isWall(right) == False and isVisited(visitedCoordinates, right) == False and right != startCoordinates:
             available.append(right)
             availableCount = availableCount + 1
-        if isWall(left) == False:
+        if isWall(left) == False and isVisited(visitedCoordinates, left) == False and left != startCoordinates:
             available.append(left)
             availableCount = availableCount + 1
-        if isWall(up) == False:
+        if isWall(up) == False and isVisited(visitedCoordinates, up) == False and up != startCoordinates:
             available.append(up)
             availableCount = availableCount + 1
-        if isWall(down) == False:
+        if isWall(down) == False and isVisited(visitedCoordinates, down) == False and down != startCoordinates:
             available.append(down)
             availableCount = availableCount + 1
+        if availableCount == 0:
+            currentCoordinates = path.pop()
+            continue
         currentCoordinates = available[random.randint(0, availableCount - 1)]
+        visitedCoordinates.append(currentCoordinates)
+        path.append(currentCoordinates)
 
 
-readLab()
+readLabirinth()
+getDimensions()
 startCoordinates = findStart()
 endCoordinates = findEnd()
-print(endCoordinates)
 wallCoordinates = findWalls()
-randomWalk()
+wholePath = randomWalk()
+drawSolution()
